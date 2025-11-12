561f7b9e9bad75a30c48f1e35fa4fc8797347443
Y0-0N63 <boyun1625@naver.com>
2025-11-12 12:21:07 +0900

    입출금하기 코드 로직 수정

--- 3_Javascript/js/04_입출금하기.js
+++ 3_Javascript/js/04_입출금하기.js
@@ -28,13 +28,22 @@ const amount = document.getElementById("amount");
 // 잔액 기록 변수 (초기값 10000)
 let balance = 10000;
 // 비밀번호 저장 변수 (초기 비밀번호 1234)
-const password = "1234";
+const password = '1234';
+// 잔액 출력 span 요소
+const output = document.getElementById("output");
+output.innerText = balance;
 
 // 입금 (input에 입력된 금액 만큼 잔액(balance)에 추가)
 function deposit() {
-  balance += Number(amount.value);
-  console.log("입금 후 금액 : ", balance);
-  document.getElementById("output").innerText = balance;
+  // input에 입력된 금액이 없다면 > 알림
+  if (amount.value.length == 0) {
+    alert("금액을 입력해주세요.");
+  } else {
+    // input에 금액이 입력되었다면 > balance에 누적
+    balance += Number(amount.value);
+    output.innerText = balance;
+    amount.value = '';
+  }
 }
 
 // 출금 (출금 버튼 클릭 시 prompt를 이용해 비밀번호를 입력 받기)
@@ -44,25 +53,28 @@ function withdrawal() {
   // 비밀번호 입력을 취소했을 경우
   if (password == null) {
     alert("비밀번호 입력을 취소했습니다.");
+    amount.value = '';
   } else {
     // 비밀번호가 일치할 때
-    if ((password == "1234")) {
+    if (password == "1234") {
       let withdrawalAmount = Number(amount.value);
       // 출금할 금액 > 잔액
       if (withdrawalAmount > balance) {
         alert("출금 금액이 잔액보다 클 수 없습니다.");
-        // 출금할 금액 <= 잔액
+        amount.value = '';
       } else {
+        // 출금할 금액 <= 잔액
         balance -= withdrawalAmount;
-        console.log("출금 후 금액 : ", balance);
-        document.getElementById("output").innerText = balance;
-        alert(
-          withdrawalAmount + '원이 출금되었습니다. 남은 잔액 : ' + balance + '원'
-        );
+        output.innerText = balance;
+        // 탬플릿 리터럴(Template Literal)
+        // 백틱(`)을 사용해 문자열을 감싸고, ${} 안에 변수/값을 넣어 동적으로 문자열을 생성하는 방식
+        alert(`${withdrawalAmount}원이 출금되었습니다. 남은 잔액 : ${balance}원`);
+        amount.value = '';
       }
       // 비밀번호 불일치
     } else {
       alert("비밀번호가 일치하지 않습니다.");
+      amount.value = '';
     }
   }
 }
